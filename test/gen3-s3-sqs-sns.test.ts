0db31d256763c22b34d2fd90e22f96ebd8edec4c
Guerdon Mukama <gmukama@gmail.com>
2025-02-25 10:08:37 +1100

    Initial commit

---
+++ test/gen3-s3-sqs-sns.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Gen3S3SqsSns from '../lib/gen3-s3-sqs-sns-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gen3-s3-sqs-sns-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Gen3S3SqsSns.Gen3S3SqsSnsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
